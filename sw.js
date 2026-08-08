be193521cebbaffb9aeeda796be72e629584b938
rajendiranr770-sudo <rajendiranr770@gmail.com>
2026-08-08 06:20:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+const CACHE='jacky-ai-v3';const A=['./','./index.html','./style.css','./app.js?v=3','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request))));
